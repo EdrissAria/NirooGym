6ce9e474058bf630e2f6ad260ee1102e3efa34dc
Edriss563 <edrissalokozay77@gmail.com>
2021-11-17 00:20:29 -0800

    working on login style

--- FRONTEND/niroo/src/Pages/Login.js
+++ FRONTEND/niroo/src/Pages/Login.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 function Login() {
     return (
-        <>
-            <canvas id="canvas"></canvas>
-        </>
+        <div className="login_container">
+            <div className="login_card">
+                <div className="login_header"><h1>Login</h1></div>
+                <form className="login_form">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username" className="login_input" placeholder="username"/>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" className="login_input" placeholder="password"/>
+                    <button type="submit" className="login_submit">Login</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
