bac3203b801b0730e15d3cc15ebff13077900aba
Edriss563 <edrissalokozay77@gmail.com>
2021-11-19 23:49:44 -0800

    login style has been completed

--- FRONTEND/niroo/src/Pages/Login.js
+++ FRONTEND/niroo/src/Pages/Login.js
@@ -4,16 +4,18 @@ function Login() {
 
     return (
         <div className="login_container">
-        <div className="circle"></div>
+        
             <div className="login_card">
+             <div className="circle"></div>
                 <div className="row">
                     <div className="col-lg-6">
                         <div className="login_left">
-                            
+                           <h1>Welcome Back</h1>
+                           <p>please login to continue</p>
                         </div>
                     </div>
                     <div className="col-lg-6">
-                        <div className="image_container"><img src="assets/img/bishak.png" alt="login_image" className="login_image" /></div>
+                        <div className="image_container"><img src="assets/img/profile.jpg" alt="login_image" className="login_image" /></div>
                         <form className="login_form">
                             <div className="input_group">
                                 <img src="assets/img/username.png" />
